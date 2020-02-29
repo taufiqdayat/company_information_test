08d14e7a9b2842c656abeeb55cdfcdcbb3f01ef7
taufiq <mtaufiq080@gmail.com>
2020-02-29 18:45:56 +0700

    push heroku

---
+++ build/precache-manifest.83b9a846c4f3595eccbb2a3c40d38cd9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0dace49ba857ebfd7bb172fe5e8f04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bec3d49bd28b225892b",
+    "url": "/static/css/2.bcd88177.chunk.css"
+  },
+  {
+    "revision": "d83938602b6e54e6b1c7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4bec3d49bd28b225892b",
+    "url": "/static/js/2.0b00bc0a.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.0b00bc0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d83938602b6e54e6b1c7",
+    "url": "/static/js/main.44e62b47.chunk.js"
+  },
+  {
+    "revision": "8aa90aedaa4814435bc0",
+    "url": "/static/js/runtime-main.274040c0.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
